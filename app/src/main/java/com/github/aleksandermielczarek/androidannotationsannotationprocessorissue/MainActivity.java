abac56f51fc591f8c759c64c255c18c9bb9afbd7
Aleksander Mielczarek <alekmiel91@gmail.com>
2017-03-08 20:32:16 +0100

    Initial commit

---
+++ app/src/main/java/com/github/aleksandermielczarek/androidannotationsannotationprocessorissue/MainActivity.java
@@ -0,0 +1,12 @@
+package com.github.aleksandermielczarek.androidannotationsannotationprocessorissue;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by Aleksander Mielczarek on 08.03.2017.
+ */
+@EActivity
+public class MainActivity extends AppCompatActivity {
+}
